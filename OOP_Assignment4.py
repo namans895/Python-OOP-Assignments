772347b55b5c4c69d45fab4f51819cd6787dbefb
namans895 <s895naman@gmail.com>
2026-06-02 01:02:28 +0530

    Update OOP_Assignment4.py

--- OOP_Assignment4.py
+++ OOP_Assignment4.py
@@ -1,1 +1,23 @@
+# Vehicle System using Polymorphism
 
+class Vehicle:
+    def start(self):
+        print("Vehicle starts")
+
+
+class Car(Vehicle):
+    def start(self):
+        print("Car starts with key")
+
+
+class Bike(Vehicle):
+    def start(self):
+        print("Bike starts with self-start")
+
+
+# Creating objects
+c1 = Car()
+b1 = Bike()
+
+c1.start()
+b1.start()
