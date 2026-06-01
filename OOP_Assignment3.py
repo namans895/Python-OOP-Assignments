7fa0e130a4b5b358f2007fa7d63db6c23aebcd65
namans895 <s895naman@gmail.com>
2026-06-02 01:02:07 +0530

    Update OOP_Assignment3.py

--- OOP_Assignment3.py
+++ OOP_Assignment3.py
@@ -1,1 +1,17 @@
+# Employee Management using Inheritance
 
+class Employee:
+    def work(self):
+        print("Employee is working")
+
+
+class Manager(Employee):
+    def manage(self):
+        print("Manager is managing the team")
+
+
+# Creating object
+m1 = Manager()
+
+m1.work()
+m1.manage()
