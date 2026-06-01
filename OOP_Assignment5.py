daf08d66eb789e2a3fd27622a628141b5004c6cf
namans895 <s895naman@gmail.com>
2026-06-02 01:02:48 +0530

    Update OOP_Assignment5.py

--- OOP_Assignment5.py
+++ OOP_Assignment5.py
@@ -1,1 +1,24 @@
+# Shape Area Calculator using Abstraction
 
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+
+# Creating object
+r1 = Rectangle(10, 5)
+
+print("Area of Rectangle =", r1.area())
